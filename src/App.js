f9d96d431635986148fe17cb30da000398edaa7f
nicolas <thelastnicko@gmail.com>
2023-03-12 11:06:45 -0400

    add basic functionality to some components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import SongList from "./components/SongList/SongList";
+import Artist from "./components/Artist/Artist"; // import the new component
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/songlist" element={<SongList />} />
+        <Route path="/artist" element={<Artist />} />
       </Routes>
     </Router>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav>
+      <h1>Karaoke Rock and Beer</h1>
+
       <ul>
         <li>
           <Link to="/">Home</Link>
